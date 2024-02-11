a040c2ae433d89041e65ae7de21289e221d7bd2e
Lilian Shaddod <lilianshaddod@gmail.com>
2024-02-11 04:19:02 +0400

    master: sending asynchronous HTTP Requests with Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 // import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 function App() {
-  // const cartItems = useSelector((state) => state.cart.itemsList);
-  // console.log(cartItems);
+  const cart = useSelector(state => state.cart);
+  useEffect(() => {
+    const sendRequest = async () => {
+      const res = await fetch("https://redux-http-c7955-default-rtdb.firebaseio.com/cartItems.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart)
+      });
+      const data = await res.json();
+    };
+    sendRequest();
+  }, [cart]);
   return (
     <div className="App">
       {/* <Auth /> */}
